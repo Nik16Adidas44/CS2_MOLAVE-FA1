a3fad1753a84037f91de3c8c0b277b13ede85e8f
Nik16Adidas44 <smmakmad31@src.pshs.edu.ph>
2026-08-13 14:00:52 +0800

    Update and rename CS2 _FA1AKMAD I DID IT MAAM.py to CS2 _FA1-AKMAD.py

--- CS2 _FA1-AKMAD.py
+++ CS2 _FA1-AKMAD.py
@@ -1,12 +1,21 @@
 ## Using math library to calculate distance of a mathematical problem
+
 import math
 x1 = float(input("Enter the first x coordinate"))
+
 x2 = float(input("Enter the second x coordinate"))
+
 y1 = float(input("Enter the first y coordinate"))
+
 y2 = float(input("Enter the second y coordinate"))
+
+
 ## input the desired values that you need to calculate ##
+
 distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
 print("The distance between the two coordinates is: {distance}")
+
 print("#the use of the library is to be more efficient when solving on code#")
 
 
