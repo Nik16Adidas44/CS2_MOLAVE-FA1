5bf0b45c3030e440bb19ba774dcb135c55a69e00
Nik16Adidas44 <smmakmad31@src.pshs.edu.ph>
2026-08-13 14:12:28 +0800

    Refactor distance calculation code in CS2_FAI-AKMAD.py

--- CS2 _FA1-AKMAD.py
+++ CS2 _FA1-AKMAD.py
@@ -12,7 +12,7 @@ y2 = float(input("Enter the second y coordinate"))
 
 ## input the desired values that you need to calculate ##
 
-distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+distance = math.sqrt ((x2 - x1)**2 + (y2 - y1)**2))
 
 print("The distance between the two coordinates is: {distance}")
 
